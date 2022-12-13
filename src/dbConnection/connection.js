7068d6b67e931562f4855560af94eb251573d8b7
marvin1604 <walter.rodriguez1992@gmail.com>
2022-12-13 14:11:32 -0500

    config deploy

--- src/dbConnection/connection.js
+++ src/dbConnection/connection.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose")
 
 const password = '1115Luciana'
 const dbname = 'cumtomerscoops'
-const uri = `mongodb+srv://walter-rodriguez-dev:${password}@cluster0.btzu7rp.mongodb.net/${dbname}?retryWrites=true&w=majority`
-// const uri2 = "mongodb://localhost:27017/fruitsDB"
+// const uri = `mongodb+srv://walter-rodriguez-dev:${password}@cluster0.btzu7rp.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+const uri2 = 'mongodb://mongo:8mFdCt953a9ZYlr4RG1H@containers-us-west-155.railway.app:7055';
 
 mongoose.set('strictQuery', false);
 
-module.exports = ()=> mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+module.exports = ()=> mongoose.connect(uri2, {useNewUrlParser: true, useUnifiedTopology: true})
 
